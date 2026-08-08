4b856b32f171be93afae837eb1ba2a0136f70e51
Tim Hawes <trhawes@gmail.com>
2026-08-07 23:36:01 -0400

    Initial comit

---
+++ src/clj_infisical/core.clj
@@ -0,0 +1,6 @@
+(ns clj-infisical.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_infisical/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-infisical.core-test
+  (:require [clojure.test :refer :all]
+            [clj-infisical.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
